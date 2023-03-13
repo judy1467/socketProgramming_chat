a4a814901593be36febb47975813650967ab7ba7
judy1467 <judy1467@naver.com>
2023-03-13 13:41:01 +0900

    test version, server and multi client connect, data send, recv success(not perfect code)

--- server.c
+++ server.c
@@ -6,21 +6,26 @@
 #include "netinet/in.h"
 #include "sys/socket.h"
 #include <arpa/inet.h>
+#define LIST_SIZE 5
 
 void error_handling(char *message);
 void *recv_thread();
+void *recv_thread1();
+void *recv_thread2();
+void *accept_thread();
 
-// sockaddr_in 구조체는 sa_family(주소체계)가 AF_INET(IPv4)인 경우에 사용
-struct sockaddr_in serv_addr, clnt_addr;
-
-// socklen_t 는 소켓 관련 매개 변수에 사용됨
-socklen_t clnt_addr_size;
-int serv_sock, clnt_sock;
+struct sockaddr_in serv_addr;
+struct sockaddr_in clnt_addr_list[LIST_SIZE];
+socklen_t clnt_addr_size_list[LIST_SIZE];
+int serv_sock;
+int clnt_sock_list[LIST_SIZE];
 char recv_data[1024];
 char send_data[1024];
-pthread_t th0;
+pthread_t th_accept;
+pthread_t th_list[LIST_SIZE];
 
 int status_exit = 0;
+int cnt_clnt = 0;
 
 int main(int argc, char* argv[]){
 
@@ -34,47 +39,43 @@ int main(int argc, char* argv[]){
     if(memset(&serv_addr, 0, sizeof(serv_addr)) == NULL)
         error_handling("memset Error!");
 
-    // sin_family는 addr, port와는 달리 네트워크를 통해 전송하는 변수가 아니라 커널에 의해서만 사용되는 변수이기 때문에 Host-Byte-Order순서가 맞다.
     serv_addr.sin_family=AF_INET;
-
-    // htonl 은 32비트의 Host-Byte-Order(Little Endian)로 부터 TCP/IP에서 사용되는 Network-Byte-Order(Big Endian)로 변환하는 함수이다.
-    // ntohl 은 htonl의 반대개념 (Network-Byte-Order -> Host-Byte-Order)
-    // INADDR_ANY: 자동으로 이 컴퓨터에 존재하는 랜카드 중 사용가능한 랜카드의 IP주소를 사용하라는 의미.
     serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
     serv_addr.sin_port=htons(atoi(argv[1]));
 
     if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
         error_handling("bind error");
 
-    // 2번째 인자는 클라이언트를 대기시킬 수 있는 큐의 크기
     if(listen(serv_sock, 5) == -1)
         error_handling("listen error");
 
-    clnt_addr_size = sizeof(clnt_addr);
+//    clnt_addr_size = sizeof(clnt_addr);
+//
+//    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+//    if(clnt_sock == -1)
+//        error_handling("accept error");
 
-    // accept의 반환값은 실패 시 -1, 성공 시 새로운 '디스크립터 번호'이다.
-    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-    if(clnt_sock == -1)
-        error_handling("accept error");
+    pthread_create(&th_accept, NULL, accept_thread, NULL);
 
-    char msg[] = "Hello this is test chat server!\nIf you want to exit the chat, type [quit] on the screen.\n";
-    write(clnt_sock, msg, sizeof(msg));
-    printf("Hello this is test chat server!\n");
-    printf("If you want to exit the chat, type [quit] on the screen.\n");
-
-    // recv쓰레드 생성
-    pthread_create(&th0, NULL, recv_thread, NULL);
+//    char msg[] = "Hello this is test chat server!\nIf you want to exit the chat, type [quit] on the screen.\n";
+//    write(clnt_sock, msg, sizeof(msg));
+//    printf("Hello this is test chat server!\n");
+//    printf("If you want to exit the chat, type [quit] on the screen.\n");
 
     while(1){
         if(status_exit)
             break;
         fgets(send_data, sizeof(send_data), stdin);
-        send(clnt_sock, send_data, sizeof(send_data), 0);
+        for(int i = 0 ; i < cnt_clnt +1; i++){
+            send(clnt_sock_list[i], send_data, sizeof(send_data), 0);
+        }
         if(strcmp(send_data, "quit") == 10)
             break;
     }
 
-    close(clnt_sock);
+    for(int i = 0 ; i < cnt_clnt ; i++){
+        close(clnt_sock_list[i]);
+    }
     close(serv_sock);
 
     return 0;
@@ -86,21 +87,48 @@ void error_handling(char *message){
     exit(1);
 }
 
-void *recv_thread(){
+void *recv_thread1(){
     while(1){
         memset(recv_data, 0 , sizeof(recv_data));
-        if(recv(clnt_sock, recv_data, sizeof(recv_data), 0) == -1){
-            printf("[disconnect!!]\n");
-            return NULL;
-        }
-        else{
-            if(strcmp(recv_data, "quit") == 10){
-                printf("[disconnect!!]\n");
-                break;
-            }
-            printf("\nclient[%s]: %s\n", inet_ntoa(clnt_addr.sin_addr), recv_data);
+        if(recv(clnt_sock_list[0], recv_data, sizeof(recv_data), 0) != -1){
+            printf("\nclient[%s:%d]: %s\n", inet_ntoa(clnt_addr_list[0].sin_addr), clnt_addr_list[0].sin_port, recv_data);
         }
     }
     status_exit = 1;
-    return NULL;
+}
+
+void *recv_thread2(){
+    while(1){
+        memset(recv_data, 0 , sizeof(recv_data));
+        if(recv(clnt_sock_list[1], recv_data, sizeof(recv_data), 0) != -1){
+            printf("\nclient[%s:%d]: %s\n", inet_ntoa(clnt_addr_list[1].sin_addr), clnt_addr_list[1].sin_port, recv_data);
+        }
+    }
+    status_exit = 1;
+}
+
+//void *recv_thread(){
+//    const int index = cnt_clnt;
+//    while(1){
+//        memset(recv_data, 0, sizeof(recv_data));
+//        if(recv(clnt_sock_list[index], recv_data, sizeof(recv_data), 0) != -1){
+//            printf("\nclient[%s:%d]: %s\n", inet_ntoa(clnt_addr_list[1].sin_addr), clnt_addr_list[1].sin_port, recv_data);
+//        }
+//    }
+//}
+
+void *accept_thread(){
+    while(1){
+        if(cnt_clnt > 0){
+            if((clnt_sock_list[cnt_clnt] = accept(serv_sock, (struct sockaddr*)&clnt_addr_list[cnt_clnt], &clnt_addr_size_list[cnt_clnt])) != -1){
+                cnt_clnt += 1;
+                pthread_create(&th_list[cnt_clnt-1], NULL, &recv_thread2, NULL);
+            }
+        }
+        printf("check: %d\n", cnt_clnt);
+        if((clnt_sock_list[cnt_clnt] = accept(serv_sock, (struct sockaddr*)&clnt_addr_list[cnt_clnt], &clnt_addr_size_list[cnt_clnt])) != -1){
+            cnt_clnt += 1;
+            pthread_create(&th_list[cnt_clnt-1], NULL, &recv_thread1, NULL);
+        }
+    }
 }
