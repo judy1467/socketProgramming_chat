e83a9e3a935fbca578f57118d8cc5d85b6f45d4d
judy1467 <judy1467@naver.com>
2023-03-18 23:03:41 +0900

    add client name, bug fix

--- client.c
+++ client.c
@@ -6,12 +6,13 @@
 #include "sys/socket.h"
 #include "pthread.h"
 
+void send_to_server();
 void error_handling(char *message);
 void *recv_thread();
 
-struct sockaddr_in serv_addr;
-socklen_t serv_addr_size;
-int clnt_sock;
+struct sockaddr_in server_addr;
+socklen_t server_addr_size;
+int client_sock;
 char recv_data[1024];
 char send_data[1024];
 pthread_t th0;
@@ -24,37 +25,36 @@ int main(int argc, char* argv[]) {
         error_handling("usage: ./filename [ip] [port]");
 
     // 소켓 생성&설정
-    clnt_sock = socket(PF_INET,SOCK_STREAM,0);
-    if(clnt_sock == -1)
+    client_sock = socket(PF_INET,SOCK_STREAM,0);
+    if(client_sock == -1)
         error_handling("socket error");
 
     // 인자를 변수에 할당
     char *ip = argv[1];
     in_port_t port = atoi(argv[2]);
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(ip);
-    serv_addr.sin_port = htons(port);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
 
-    serv_addr_size = sizeof(serv_addr);
+    server_addr_size = sizeof(server_addr);
 
-    if(connect(clnt_sock, (struct sockaddr *) &serv_addr, serv_addr_size) == -1)
+    if(connect(client_sock, (struct sockaddr *) &server_addr, server_addr_size) == -1)
         error_handling("connect error");
+
+    printf("Enter your name: ");
+    fgets(send_data, sizeof(send_data), stdin);
+    send(client_sock, send_data, sizeof(send_data), 0);
+    memset(send_data, 0, sizeof(send_data));
 
     pthread_create(&th0, NULL, recv_thread, NULL);
 
-    while(1){
-        if(status_exit)
-            break;
-        fgets(send_data, sizeof(send_data), stdin);
-        send(clnt_sock, send_data, sizeof(send_data), 0);
-        if(strcmp(send_data, "quit") == 10)
-            break;
-        memset(send_data, 0, sizeof(send_data));
-    }
+    send_to_server();
 
-    close(clnt_sock);
+    pthread_cancel(th0);
+    pthread_join(th0, (void **)NULL);
+    close(client_sock);
 
     return 0;
 }
@@ -65,18 +65,34 @@ void error_handling(char *message){
     exit(1);
 }
 
+void send_to_server(){
+    while(1){
+        if(status_exit)
+            break;
+        fgets(send_data, sizeof(send_data), stdin);
+        if(strcmp(send_data, "") == 10){
+            continue;
+        }
+        printf("\n");
+        send(client_sock, send_data, sizeof(send_data), 0);
+        if(strcmp(send_data, "quit") == 10)
+            break;
+        memset(send_data, 0, sizeof(send_data));
+    }
+}
+
 void *recv_thread(){
     while(1){
-        if(recv(clnt_sock, recv_data, sizeof(recv_data), 0) == -1){
+        if(recv(client_sock, recv_data, sizeof(recv_data), 0) == -1){
             printf("[disconnect!!]\n");
             return NULL;
         }
         else{
-            if(strcmp(recv_data, "quit") == 10){
+            if(strcmp(recv_data, "server: quit") == 10){
                 printf("[disconnect!!]\n");
                 break;
             }
-            printf("%s", recv_data);
+            printf("%s\n", recv_data);
             memset(recv_data, 0, sizeof(recv_data));
         }
     }
--- server.c
+++ server.c
@@ -8,11 +8,13 @@
 #include <arpa/inet.h>
 #define LIST_SIZE 5
 
+void send_to_client();
 void error_handling(char *message);
 void *recv_thread(const int *arg);
 void *accept_thread();
 void test();
 void convert(int index);
+void eliminate_char(char* str, char c);
 
 struct sockaddr_in serv_addr;
 struct sockaddr_in clnt_addr_list[LIST_SIZE];
@@ -24,6 +26,7 @@ char send_data[1024];
 char convert_recv_data[1024];
 pthread_t th_accept;
 pthread_t th_list[LIST_SIZE];
+char client_name[LIST_SIZE][1024];
 
 int status_exit = 0;
 int cnt_clnt = 0;
@@ -48,28 +51,18 @@ int main(int argc, char* argv[]){
     if(listen(serv_sock, 5) == -1)
         error_handling("listen error");
 
-    pthread_create(&th_accept, NULL, accept_thread, NULL);
-
-    while(cnt_clnt == 0){}
-
     printf("Hello this is test chat server!\n");
     printf("If you want to exit the chat, type [quit] on the screen.\n");
+    printf("waiting client...\n");
 
-    while(!status_exit){ // send to all connected client
-        fgets(send_data, sizeof(send_data), stdin);
-        if(strcmp(send_data, "test") == 10){
-            test();
-            continue;
-        }
-        else if(strcmp(send_data, "quit") == 10)
-            break;
-        for(int i = 0 ; i < cnt_clnt ; i++)
-            send(clnt_sock_list[i], send_data, sizeof(send_data), 0);
-        memset(send_data, 0, sizeof(send_data));
-    }
+    pthread_create(&th_accept, NULL, accept_thread, NULL);
 
-    for(int i = 0 ; i < cnt_clnt ; i++){
-        close(clnt_sock_list[i]);
+    while(cnt_clnt == 0){} // wait for connection with client
+
+    send_to_client();
+
+    for(int i = 0 ; i < LIST_SIZE ; i++){
+        pthread_join(th_list[i], (void**)NULL);
     }
     close(serv_sock);
 
@@ -82,35 +75,70 @@ void error_handling(char *message){
     exit(1);
 }
 
+void send_to_client(){
+    while(!status_exit){ // send to all connected client
+        if(cnt_clnt == 0){
+            break;
+        }
+        strcat(send_data, "server: ");
+        fgets(send_data+8, sizeof(send_data)-8, stdin);
+        if(strcmp(send_data, "server: test") == 10){
+            test();
+            continue;
+        }
+        else if(strcmp(send_data, "server: quit") == 10){
+            for(int i = 0 ; i < LIST_SIZE ; i++)
+                send(clnt_sock_list[i], send_data, sizeof(send_data), 0);
+
+            pthread_cancel(th_accept);
+
+            for(int i = 0 ; i < LIST_SIZE ; i++){
+                pthread_cancel(th_list[i]);
+            }
+            break;
+        }
+        else if(strcmp(send_data, "server: ") == 10){
+            continue;
+        }
+        for(int i = 0 ; i < LIST_SIZE ; i++)
+            send(clnt_sock_list[i], send_data, sizeof(send_data), 0);
+        memset(send_data, 0, sizeof(send_data));
+    }
+}
+
 void *recv_thread(const int *arg){
+    int first_data = 0;
     const int index = *arg;
     status_thread_done = 1;
     while(1){
         if(recv(clnt_sock_list[index], recv_data, sizeof(recv_data), 0) != -1){
-            printf("\nclient[%s:%d, index: %d]: %s\n", inet_ntoa(clnt_addr_list[index].sin_addr), clnt_addr_list[index].sin_port, index, recv_data);
+            if(!first_data){
+                memcpy(client_name[index], recv_data, sizeof(recv_data));
+                eliminate_char(client_name[index], '\n');
+                first_data = 1;
+                continue;
+            }
+
+            if(strcmp(recv_data, "quit") == 10){
+                close(clnt_sock_list[index]);
+                printf("[%s] is out of this server.", client_name[index]);
+                cnt_clnt--;
+                break;
+            }
+
+            printf("\n%s[%s:%d, index: %d]: %s\n", client_name[index], inet_ntoa(clnt_addr_list[index].sin_addr), clnt_addr_list[index].sin_port, index, recv_data);
 
             convert(index);
-            for(int i = 0 ; i < cnt_clnt ; i++){
+            for(int i = 0 ; i < LIST_SIZE ; i++){
                 if(i != index)
                     send(clnt_sock_list[i], convert_recv_data, sizeof(convert_recv_data), 0);
             }
 
             memset(convert_recv_data, 0, sizeof(convert_recv_data));
             memset(recv_data, 0, sizeof(recv_data));
-
-            if(strcmp(recv_data, "quit") == 10){
-                if(cnt_clnt > 1){
-                    char msg[] = "quit\n";
-                    for(int i = 0 ; i < cnt_clnt ; i++){
-                        write(clnt_sock_list[i], msg, sizeof(msg));
-                    }
-                }
-                printf("[disconnect!!]");
-                status_exit = 1;
-                break;
-            }
         }
     }
+    pthread_cancel(th_list[index]);
     return NULL;
 }
 
@@ -119,7 +147,7 @@ void *accept_thread(){
         clnt_addr_size_list[cnt_clnt] = (int)sizeof(clnt_addr_list[cnt_clnt]);
         if((clnt_sock_list[cnt_clnt] = accept(serv_sock, (struct sockaddr *) &clnt_addr_list[cnt_clnt], &clnt_addr_size_list[cnt_clnt])) != -1){
 
-            char msg[] = "Hello this is test chat server!\nIf you want to exit the chat, type [quit] on the screen.\n";
+            char msg[] = "Hello! This is test chat server!\nIf you want to exit the chat, type [quit] on the screen.\n";
             write(clnt_sock_list[cnt_clnt], msg, sizeof(msg));
 
             printf("[new client enter], cnt_clnt: %d\n", cnt_clnt+1);
@@ -130,6 +158,7 @@ void *accept_thread(){
             ++cnt_clnt;
         }
     }
+    pthread_cancel(th_accept);
     return NULL;
 }
 
@@ -150,6 +179,7 @@ void test(){ // for check client information
 }
 
 void convert(int index){
+    strcat(convert_recv_data, client_name[index]);
     strcat(convert_recv_data, "[");
     strcat(convert_recv_data, inet_ntoa(clnt_addr_list[index].sin_addr));
     strcat(convert_recv_data, ":");
@@ -160,4 +190,13 @@ void convert(int index){
     strcat(convert_recv_data, temp);
     strcat(convert_recv_data, "]: ");
     strcat(convert_recv_data, recv_data);
+}
+
+void eliminate_char(char* str, char c){
+    for(; *str != '\0' ; str++){
+        if(*str == c){
+            strcpy(str, str+1);
+            str--;
+        }
+    }
 }
